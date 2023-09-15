bf7b7f4a2ac5dff9340ae6544fc71298b9418f8a
Sawkon <aaronjunxiang.chan@digipen.edu>
2023-09-15 12:45:11 +0800

    coordinate system in global

--- Oui Uninteractive/Oui Uninteractive/Include/Global.h
+++ Oui Uninteractive/Oui Uninteractive/Include/Global.h
@@ -2,12 +2,77 @@
 // includes
 #include <iostream>;
 
-// structs
 
-class coordinates {
+/**************************************************************************
+ * @brief A class representing 2D coordinates.
+ *
+ * The Coordinates class allows you to work with 2D coordinates,
+ * providing methods to set, get, and modify the x and y coordinates.
+ *************************************************************************/
+class Coordinates {
 public:
-	float xCoords, yCoords;
-	bool SetCoords();
-private:
 
+    /**************************************************************************
+     * @brief Enum representing coordinate indices.
+     *
+     * The CoordinateSelector enum provides named constants for selecting
+     * specific coordinates within a Coordinates object. It is used to
+     * specify whether to access the x-coordinate or the y-coordinate.
+     *************************************************************************/
+    enum CoordinateSelector {
+        X_INDEX, Y_INDEX
+    };
+
+
+    /**************************************************************************
+     * @brief Constructor for the Coordinates class.
+     *
+     * This constructor initializes a Coordinates object with the specified
+     * x and y coordinate values.
+     *
+     * @param x The initial x-coordinate value.
+     * @param y The initial y-coordinate value.
+     *************************************************************************/
+    Coordinates(float x, float y) : xCoords(x), yCoords(y) {}
+
+    /**************************************************************************
+     * @brief Get the specified coordinate value.
+     *
+     * This function returns either the x-coordinate or the y-coordinate of the
+     * Coordinates object based on the provided coordinate index.
+     *
+     * @param coordIndex A boolean flag indicating which coordinate to retrieve.
+     *                   Use `true` for the x-coordinate and `false` for the
+                         y-coordinate.
+     * @return The requested coordinate value as a float.
+     *************************************************************************/
+    float GetCoords(bool coordIndex) const;
+
+    /**************************************************************************
+     * @brief Set the coordinates to the specified values.
+     *
+     * This function allows you to set both the x and y coordinates of the Coordinates
+     * object to the specified values.
+     *
+     * @param x The new x-coordinate value.
+     * @param y The new y-coordinate value.
+     *************************************************************************/
+    void SetCoords(float x, float y);
+
+    /**************************************************************************
+     * @brief Add a value to the specified coordinate.
+     *
+     * This function allows you to add a value to either the x or y coordinate
+     * of the Coordinates object.
+     *
+     * @param coordIndex A boolean flag indicating the coordinate to modify.
+     *                   Use `true` for the x-coordinate and `false` for the
+     *                   y-coordinate.
+     * @param val        The value to add to the selected coordinate.
+     *************************************************************************/
+    void AddCoords(bool coordIndex, float val);
+
+private:
+    float xCoords, yCoords;
 };
+
