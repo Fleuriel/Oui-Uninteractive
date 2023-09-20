6d9c2c9a285c73a8cdb5707fcdec6fe5a0972645
EmeryQuek <c.quek@digipen.edu>
2023-09-20 13:22:41 +0800

    Collision static circle and rectangle

--- Oui Uninteractive/Oui Uninteractive/Include/Collision.h
+++ Oui Uninteractive/Oui Uninteractive/Include/Collision.h
@@ -1,1 +1,4 @@
 #pragma once
+
+bool CollisionStaticCircleCircle(Coordinates Coords1, Coordinates Coords2, float r1, float r2);
+bool CollisionStaticRectRect(Coordinates Coords1, Coordinates Coords2, float s1, float s2);
--- Oui Uninteractive/Oui Uninteractive/Source/Collision.cpp
+++ Oui Uninteractive/Oui Uninteractive/Source/Collision.cpp
@@ -1,7 +1,68 @@
 #include "Collision.h"
+#include <math.h>
+#include "Vector2D.h"
+#include <CoordinateSystem.h>
 
 
-//Circle-Circle?
-//bool collisionCircleCircle(Vector2DDistance(x,y), Radius of A, Radius of B)
-// 
-//
+/**************************************************************************
+ * @brief Check for collision between static circles
+ *
+ * This function returns true or false depending on whether collision is
+ * detected or not
+ *
+ * @param Vector2DDistance The distance between two the two coordinates
+ * @param r1 Radius of the first circle
+ * @param r2 Radius of the second circle
+ *************************************************************************/
+bool CollisionStaticCircleCircle(Coordinates Coords1, Coordinates Coords2, float r1=2, float r2=2)
+{
+	float RadiusTotal = r1 + r2;
+	return Coords1.DistanceIsWithinThreshold(Coords2, (RadiusTotal));
+}
+
+/**************************************************************************
+ * @brief Check for collision between static rectangles
+ *
+ * This function returns true or false depending on whether collision is
+ * detected or not
+ *
+ * @param Vector2DDistance The distance between two the two coordinates
+ * @param 
+ * @param 
+ *************************************************************************/
+bool CollisionStaticRectRect(Coordinates Coords1, Coordinates Coords2, float s1=3, float s2=3)
+{
+		float leftA, leftB {0};
+		leftA = Coords1.X_INDEX - (s1 / 2) ;
+		leftB = Coords2.X_INDEX - (s2 / 2) ;
+
+		float rightA, rightB {0};
+		rightA = Coords1.X_INDEX + (s1 / 2) ;
+		rightB = Coords2.X_INDEX + (s2 / 2) ;
+
+		float topA, topB {0};
+		topA = Coords1.Y_INDEX - (s1 / 2) ;
+		topB = Coords2.Y_INDEX - (s2 / 2) ;
+
+		float bottomA, bottomB {0};
+		bottomA = Coords1.Y_INDEX + (s1 / 2) ;
+		bottomB = Coords2.Y_INDEX + (s2 / 2) ;
+
+		if ((rightA > leftB  && 
+			 topA > bottomB) &&
+			(rightB > leftA  &&
+			 topB > bottomA))
+		{
+		return true;
+		}
+		else 
+		{
+		return false;
+		}
+
+		//if(leftA > rightB) then no collision
+		//if(leftB > rightA) then no collision
+		//if(topA < bottomB) then no collision
+		//if(topB < bottomA) then no collision
+
+}
