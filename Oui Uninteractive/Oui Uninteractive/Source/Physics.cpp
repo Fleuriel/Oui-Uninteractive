46b6a517557f9d4fc78bbca6ddf3419e6ff6fec2
TristanCheah <cheahtristan@Hotmail.sg>
2023-09-21 09:17:58 +0800

    Comments

--- Oui Uninteractive/Oui Uninteractive/Source/Physics.cpp
+++ Oui Uninteractive/Oui Uninteractive/Source/Physics.cpp
@@ -2,7 +2,12 @@
 #include "ComponentFactory.h"
 #include "ObjectFactory.h"
 #include <iostream>
+//initialize global pointer
 Physics* physicsSys = nullptr;
+
+/**************************************************************************
+* @brief Default constructor for Physics System
+*************************************************************************/
 Physics::Physics() {
 	if (physicsSys != nullptr) {
 		//instantiate physics system
@@ -13,6 +18,10 @@ Physics::Physics() {
 	}
 	
 }
+/**************************************************************************
+* @brief Initializes the Physics System
+* @return void
+*************************************************************************/
 void Physics::Initialize() {
 	//Register Component creator of Body here
 	ComponentFactory<PhysicsBody>* testPtr = new ComponentFactory<PhysicsBody>(ComponentType::PhysicsBody);
@@ -20,6 +29,11 @@ void Physics::Initialize() {
 	ComponentFactory<Transform>* testPtr2 = new ComponentFactory<Transform>(ComponentType::Transform);
 	objectFactory->AddComponentFactory(ComponentType::Transform, testPtr2);
 }
+/**************************************************************************
+* @brief Update Function of the Physics System
+* @param float dt - delta time
+* @return void
+*************************************************************************/
 void Physics::Update(float dt) {
 
 	for (PhysicsBody* body : bodyList) {
@@ -34,27 +48,54 @@ void Physics::Update(float dt) {
 	}
 
 }
+/**************************************************************************
+* @brief Add force to all object's Physics Body
+* @param Vec 2 force - force to be added to object
+* @return void
+*************************************************************************/
 void Physics::addForce(Vec2 force) {
 	for (PhysicsBody* body : bodyList) {
 		body->accumulatedForce += force;
 	}
 }
+/**************************************************************************
+* @brief Set the position of all object's Physics Body
+* @param Vec 2 pos - the position to be set to
+* @return void
+*************************************************************************/
 void Physics::setPosition(Vec2 pos) {
 	for (PhysicsBody* body : bodyList) {
 		body->position = pos;
 	}
 }
+/**************************************************************************
+* @brief Set the position of one object's Physics Body
+* @param Vec2 pos - the position to be set to
+* @param size_t ID - ID of object to set position
+* @return void
+*************************************************************************/
 void Physics::setPosition(Vec2 pos, size_t ID) {
 	
 	if (ID < bodyList.size()) {
 		bodyList.at(ID)->position = pos;	
 	}
 }
+/**************************************************************************
+* @brief Set the velocity of all object's Physics Body
+* @param Vec 2 newVelocity - the velocity to be set to
+* @return void
+*************************************************************************/
 void Physics::setVelocity(Vec2 newVelocity) {
 	for (PhysicsBody* body : bodyList) {
 		body->velocity = newVelocity;
 	}
 }
+/**************************************************************************
+* @brief Set the velocity of one object's Physics Body
+* @param Vec 2 newVelocity - the velocity to be set to
+* @param size_t ID - ID of object to set position
+* @return void
+*************************************************************************/
 void Physics::setVelocity(Vec2 velocity, size_t ID) {
 	if (ID < bodyList.size()) {
 		bodyList.at(ID)->velocity = velocity;
--- Oui Uninteractive/Oui Uninteractive/Source/PhysicsBody.cpp
+++ Oui Uninteractive/Oui Uninteractive/Source/PhysicsBody.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "Physics.h"
-
+/**************************************************************************
+* @brief Default constructor for PhysicsBody component
+*************************************************************************/
 PhysicsBody::PhysicsBody() {
 	position = Vec2(0, 0);
 	velocity = Vec2(0, 0);
 	acceleration = Vec2(0, 0);
 	isStatic = false;
 }
-
+/**************************************************************************
+* @brief Initialize this instance of the PhysicsBody component
+* @return void
+*************************************************************************/
 void PhysicsBody::Initialize() {
 	//Transform
 	//assign pos to transform
@@ -19,7 +24,12 @@ void PhysicsBody::Initialize() {
 	physicsSys->bodyList.push_back(this);
 
 }
-
+/**************************************************************************
+* @brief Initialize this instance of the PhysicsBody component
+* @param const std::string& filePath - file path to read from
+* @param rapidjson::Value::ConstMemberIterator& itr - iterator through json object
+* @return void
+*************************************************************************/
 void PhysicsBody::Serialize(const std::string& filePath, rapidjson::Value::ConstMemberIterator& itr) {
 	const rapidjson::Value& components{itr->value};
 	velocity.x = components["VelocityX"].GetFloat();
