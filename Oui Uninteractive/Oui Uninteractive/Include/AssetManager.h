32b27b04dfd350d229deec06e5c52c4a0b952f86
Markkeroni <112945155+Markkeroni@users.noreply.github.com>
2023-09-22 19:50:48 +0800

    Added Asset Manager

---
+++ Oui Uninteractive/Oui Uninteractive/Include/AssetManager.h
@@ -0,0 +1,17 @@
+/**************************************************************************
+ * @file AssetManager.h
+ * @author
+ * @par DP email:
+ * @par Course: CSD 2401
+ * @par Software Engineering Project 3
+ * @date 09-05-2023
+ * @brief This source file contains the code to setup and run the editor
+ *************************************************************************/
+#pragma once
+
+#include <filesystem>
+
+class AssetManager {
+public:
+	static LoadAudio()
+};
