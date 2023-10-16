ff105df51d1290f882e79d0bc94565beae7ba6b8
Austin Hwang <hwangjingrui@gmail.com>
2023-10-16 18:01:41 +0800

    Base for messaging system
    - Everything is still in 1 file
    - Incomplete declarations for the functions
    - Still need more research
    - Need to figure out how to attach the messaging stuff to the systems and components

---
+++ Oui Uninteractive/Oui Uninteractive/Include/IObserver.h
@@ -0,0 +1,26 @@
+/**************************************************************************
+ * @file IObserver.h
+ * @author HWANG Jing Rui, Austin
+ * @par DP email: jingruiaustin.hwang@digipen.edu
+ * @par Course:	CSD 2401
+ * @par Software Engineering Project 3
+ * @date 16-10-2023
+ * @brief
+ *************************************************************************/
+#ifndef IOBSERVER_H
+#define IOBSERVER_H
+
+#include <string>
+
+/*class IObserver {
+private:
+	std::string message;
+
+public:
+	IObserver() {}
+	~IObserver() {}
+
+	void HandleMessage();
+};*/
+
+#endif
