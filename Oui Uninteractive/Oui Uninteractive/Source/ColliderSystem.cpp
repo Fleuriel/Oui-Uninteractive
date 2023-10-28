8eb88cd4437c6c557e3519af1ff4b1701abe062f
TristanCheah <cheahtristan@Hotmail.sg>
2023-10-28 14:39:27 +0800

    Seems most stable

--- Oui Uninteractive/Oui Uninteractive/Source/ColliderSystem.cpp
+++ Oui Uninteractive/Oui Uninteractive/Source/ColliderSystem.cpp
@@ -66,11 +66,7 @@ void ColliderSystem::BroadPhase() {
 
 void ColliderSystem::Update(float dt) {
 	TimeProfiler profiler(Editor::timeRecorder.colliderTime);
-	std::map<size_t, Collider*>::iterator it = colliderMap.begin();
-	std::map<size_t, Collider*>::iterator it2 = colliderMap.begin();
-
-
-	for (; it != colliderMap.end(); it++) {
+	for (std::map<size_t, Collider*>::iterator it = colliderMap.begin(); it != colliderMap.end(); it++) {
 		Collider* collider = it->second;
 
 		collider->tx->position = GET_COMPONENT(collider->GetOwner(), Transform, ComponentType::TRANSFORM)->position;
@@ -79,7 +75,7 @@ void ColliderSystem::Update(float dt) {
 		collider->boundingbox->max = Vec2((0.5f) * collider->tx->scale + collider->tx->position.x, (0.5f) * collider->tx->scale + collider->tx->position.y);
 
 		
-			for (; it2 != colliderMap.end(); it2++) {
+			for (std::map<size_t, Collider*>::iterator it2 = colliderMap.begin(); it2 != colliderMap.end(); it2++) {
 				Collider* body2 = it2->second;
 				if (body2->GetOwner()->GetGameObjectID() == collider->GetOwner()->GetGameObjectID()) {
 					continue;
@@ -88,25 +84,25 @@ void ColliderSystem::Update(float dt) {
 				PhysicsBody* pBody2 = GET_COMPONENT(body2->GetOwner(), PhysicsBody, ComponentType::PHYSICS_BODY);
 				float contactTime = 0;
 				bool collided = false;
-				if (Vector2DLength(pBody1->velocity) > 0 && Vector2DLength(pBody2->velocity) > 0) {
-					Vec2 normal = Vec2(0, 0);
-					Vec2 contactPt = Vec2(0, 0);
-					collided = CollisionMovingRectRect(*(collider->boundingbox), *(body2->boundingbox), pBody1->velocity, pBody2->velocity, contactTime, normal, contactPt, GetDT());
-					//dynamic coll response
-					if (collided) {
-						Vec2 relVelocity = pBody2->velocity - pBody1->velocity;
-						//pBody1->forceManager.ApplyToForce(normal * Vec2(abs(pBody1->velocity.x), abs(pBody1->velocity.y)), (1 - contactTime), 0.25f,FORCE_INDEX::EXTERNAL);
-						pBody1->txPtr->position += normal * Vec2(abs(relVelocity.x), abs(relVelocity.y)) * GetDT() * (1 - contactTime);
+				//if (Vector2DLength(pBody1->velocity) > 0 && Vector2DLength(pBody2->velocity) > 0) {
+				Vec2 normal = Vec2(0, 0);
+				Vec2 contactPt = Vec2(0, 0);
+				collided = CollisionMovingRectRect(*(collider->boundingbox), *(body2->boundingbox), pBody1->velocity, pBody2->velocity, contactTime, normal, contactPt, GetDT());
+				//dynamic coll response
+				if (collided) {
+					Vec2 relVelocity = pBody2->velocity - pBody1->velocity;
+					//pBody1->forceManager.ApplyToForce(normal * Vec2(abs(pBody1->velocity.x), abs(pBody1->velocity.y)), (1 - contactTime), 0.25f,FORCE_INDEX::EXTERNAL);
+					pBody1->txPtr->position += normal * Vec2(abs(relVelocity.x), abs(relVelocity.y)) * GetDT() * (1 - contactTime);
 
 
-						/*w += normal * Vec2(abs(pBody1->velocity.x), abs(pBody1->velocity.y)) * (1 - contactTime);*/
-						std::cout << contactTime << "\n";
-						std::cout << normal.x << "|" << normal.y << "\n";
-					
-					}
+					/*w += normal * Vec2(abs(pBody1->velocity.x), abs(pBody1->velocity.y)) * (1 - contactTime);*/
+					std::cout << contactTime << "\n";
+					std::cout << normal.x << "|" << normal.y << "\n";
 					
 				}
-				else {
+					
+				//}
+				/*else {
 					collided = CollisionStaticDynamicRectRect(*(collider->boundingbox), *(body2->boundingbox));
 					
 					if (collided) {
@@ -118,51 +114,13 @@ void ColliderSystem::Update(float dt) {
 						ProcessMessage(&collisionMessage);
 					}
 					
-				}
+				}*/
 				
 
-				//bool staticCollided = CollisionStaticDynamicRectRect(*(collider->boundingbox), *(body2->boundingbox));
-				//float dynamicCollided = 0.f;
-				//PhysicsBody* pBody1 = GET_COMPONENT(collider->GetOwner(), PhysicsBody, ComponentType::PHYSICS_BODY);
-				//if (!staticCollided) {
-
-				//	PhysicsBody* pBody2 = GET_COMPONENT(body2->GetOwner(), PhysicsBody, ComponentType::PHYSICS_BODY);
-
-				//	float tFirst = 0.0f;
-				//	dynamicCollided = CollisionMovingRectRect(*(collider->boundingbox), *(body2->boundingbox), pBody1->velocity, pBody2->velocity, tFirst);
-				//	if (dynamicCollided) {
-				//		std::cout << "!\n";
-				//		pBody1->txPtr->position = pBody1->txPtr->previousPosition + (1 - tFirst) * pBody1->velocity * dt;
-				//	}
-				//}
-				//if (staticCollided) {
-				//	std::cout << "Static Collision\n";
-				//	Vec2 normal = Vec2(0, 0);
-				//	float depth = CalculateEntryTimeAndNormal(collider->boundingbox, body2->boundingbox, pBody1->velocity, normal.x, normal.y);
-				//	Vector2DNormalize(normal, normal);
-				//	//	CollisionMessage collisionMessage(collider, body2, depth, normal);
-				//	//	ProcessMessage(&collisionMessage);
-				//}
-				//else if (staticCollided == false && dynamicCollided == false) {
-
-				//}
+				
 			}
 		
 	}
 
-	//BroadPhase();
 
-	//Narrow Phase
- 	/*for (std::set<int> colSet : collisionData) {
-		for (int num : colSet) {
-			std::set<int>::iterator it = colSet.upper_bound(num);
-			if (it != colSet.end()) {
-				CollisionStaticDynamicRectRect(*(colliderMap[num])->boundingbox, *(colliderMap[*it])->boundingbox);
-			}
-		}
-	}*/
-
-//	}
-	//insert collision detection here
-	//response should be inside the function
-	}
+}
