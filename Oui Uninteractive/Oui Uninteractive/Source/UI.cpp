a90e70e596dc023c6aaff8f3cd19927da5de37bf
Markkeroni <112945155+Markkeroni@users.noreply.github.com>
2023-10-29 01:56:53 +0800

    Update UI

--- Oui Uninteractive/Oui Uninteractive/Source/UI.cpp
+++ Oui Uninteractive/Oui Uninteractive/Source/UI.cpp
@@ -46,21 +46,25 @@ void UIManager::Update(float dt) {
 	// Start time profiling for sound system
 	//TimeProfiler profiler(Editor::timeRecorder.soundTime);
 	
-	// Convert window mouse pos to gamewindow mousepos, and top left to middle
-	std::pair<int, int> convertedMousePos;
-	//this is the problem u dumb fk the. u not even taking the mouse position anywhere
-	convertedMousePos.first = (Editor::gameWindowSize.first - Editor::gameWindowOrigin.first) / 2;
-	convertedMousePos.second = (Editor::gameWindowSize.second - Editor::gameWindowOrigin.second) / 2;
-	// 1. Loop through game obj list
-	// 2. if its type is button
-	// 3. if its name is exit
-	// 4. if its clicked
-	// 5. Quit
-	std::map<size_t, GameObject*> copyMap = objectFactory->GetGameObjectIDMap();
-	std::map<size_t, GameObject*>::iterator it = copyMap.begin();
-	std::cout << Editor::gameWindowSize.first << " " << Editor::gameWindowSize.second << std::endl;
-	//for (; it != objectFactory->GetGameObjectIDMap().end(); it++) {
-	for (int x = 0; x < 6; x++, it++) {
+	if (mouseButtonStates[GLFW_MOUSE_BUTTON_LEFT]) {
+		std::pair<double, double> convertedMousePos;
+		double mouseX, mouseY;
+		glfwGetCursorPos(windowNew, &mouseX, &mouseY);
+
+
+		float gameWindowMouseX = mouseX - Editor::gameWindowOrigin.first;
+		float gameWindowMouseY = mouseY - Editor::gameWindowOrigin.second;
+		convertedMousePos.first = gameWindowMouseX - Editor::gameWindowSize.first * 0.5f;
+		convertedMousePos.second = (Editor::gameWindowSize.second * 0.5f) - gameWindowMouseY;
+
+		//std::cout << "converted pos: " << convertedMousePos.first << ", " << convertedMousePos.second << std::endl;
+		//std::cout << Editor::gameWindowSize.first << ", " << Editor::gameWindowSize.second << std::endl;
+
+		std::map<size_t, GameObject*> copyMap = objectFactory->GetGameObjectIDMap();
+		std::map<size_t, GameObject*>::iterator it = copyMap.begin();
+
+		//for (; it != objectFactory->GetGameObjectIDMap().end(); it++) {
+		for (int x = 0; x < 6; x++, it++) {
 			//std::cout << it->second->GetName() << std::endl;
 			if (it->second->GetType() == "Button") {
 				if (it->second->GetName() == "Exit") {
@@ -71,15 +75,9 @@ void UIManager::Update(float dt) {
 					}
 				}
 			}
-			
+
 		}
-		
-	
-	
-	
-
-
-
+	}
 	(void)dt;
 }
 
@@ -91,3 +89,41 @@ void UIManager::Update(float dt) {
 UIManager::~UIManager() {
 
 }
+
+
+/**************************************************************************
+* @brief This function maps window coordinates of the entire window into the render window
+* @param inputCoords - Input coordinates to map to
+* @param targetWindowOrigin - The Origin point offset of the target window in respect to the top left corner of the main window
+* @param wop - The window origin point of the taget window
+* @param targetWindowSize - The dimensions of the target window
+* @return Returns a pair of ints representing the mapped position
+*************************************************************************/
+//std::pair<int, int> UIManager::ConvertCoordsWindowToWindow(
+//	std::pair<int, int> inputCoords,
+//	std::pair<int, int> sourceWindowSize,
+//	std::pair<int, int> targetWindowOrigin,
+//	std::pair<int, int> targetWindowSize,
+//	UIManager::WindowOriginPoint targetWindowOP) {
+//	
+//	std::pair<int, int> result;
+//
+//	// Get offset of target window
+//	std::pair<int, int> windowOffset;
+//	windowOffset.first = targetWindowOrigin.first;
+//	windowOffset.second = targetWindowOrigin.second;
+//
+//	// For target window origin at center
+//	if (targetWindowOP == UIManager::WindowOriginPoint::CENTER) {
+//		//windowOffset.first -= targetWindowSize.first / 2;
+//		//windowOffset.second -= targetWindowSize.second / 2;
+//	}
+//	// Map input coords
+//	result.first = inputCoords.first - windowOffset.first;
+//	result.second = inputCoords.second - windowOffset.second;
+//	// Scale input coords
+//	result.first = (result.first * targetWindowSize.first) / sourceWindowSize.first;
+//	result.second = (result.second * targetWindowSize.second) / sourceWindowSize.second;
+//	return result;
+//
+//}
