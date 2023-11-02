48a18e4c8e57a70a480c6d48c744d5133e603bf1
Austin Hwang <hwangjingrui@gmail.com>
2023-11-02 12:31:04 +0800

    Pathfinding finally works yay!

--- Oui Uninteractive/Oui Uninteractive/Include/EnemyStates.h
+++ Oui Uninteractive/Oui Uninteractive/Include/EnemyStates.h
@@ -19,27 +19,29 @@
 class EnemyRoam : public IState {
 private:
 	BFS* bfs;
-	//std::vector<Node*> pathToTake;
 	std::vector<Node> pathToTake;
+	bool transitioned;
 	bool pathFound;
-	bool printTest;
-	bool wallPrinted;
-	int index;
+	bool pathPrinted;
+	int pathIndex;
 
 public:
 	//void Initialize() override;
 	EnemyRoam();
 	void Update(size_t gameObjectID) override;
+	void ExitState() override;
 	~EnemyRoam() override;
 };
 
 class EnemyAttack : public IState {
 private:
+	bool transitioned;
 
 public:
 	//void Initialize() override;
 	EnemyAttack();
 	void Update(size_t gameObjectID) override;
+	void ExitState() override;
 	~EnemyAttack() override;
 };
 
--- Oui Uninteractive/Oui Uninteractive/Source/EnemyStates.cpp
+++ Oui Uninteractive/Oui Uninteractive/Source/EnemyStates.cpp
@@ -18,154 +18,24 @@
 #include "Transform.h"
 #include "PhysicsBody.h"
 
-// FOR TESTING OF BFS //
-/*#include <vector>
-#include <queue>
-#include <algorithm>
-#include <iostream>
-
-int rows{ 10 };
-int cols{ 10 };
-std::vector<std::vector<int>> gameMap(rows, std::vector<int>(cols, 0));
-bool pathFound{ false };
-
-struct Node {
-    float x, y;
-    Node* parent;
-
-    // Node constructor
-    Node(int x_, int y_) : x(x_), y(y_), parent(nullptr) {}
-};
-
-std::vector<Node*> BFS(int startX, int startY, int targetX, int targetY) {
-    // Test walls
-    //gameMap[0][3] = 1;
-    //gameMap[1][2] = 1;
-    //gameMap[1][3] = 1;
-    //gameMap[2][3] = 1;
-    //gameMap[3][1] = 1;
-    //gameMap[3][2] = 1;
-    //gameMap[3][3] = 1;
-
-    // Queue of nodes to determine path
-    std::queue<Node*> nodeQueue;
-
-    // Path of nodes to follow
-    std::vector<Node*> path;
-
-    // Start/end nodes
-    Node* startNode{ new Node(startX, startY) };
-    Node* endNode{ new Node(targetX, targetY) };
-
-    nodeQueue.push(startNode);
-
-    while (!nodeQueue.empty()) {
-        Node* currentNode = nodeQueue.front();
-        nodeQueue.pop();
-
-        // If the current node is the end node
-        if (currentNode->x == endNode->x && currentNode->y == endNode->y) {
-            // Trace parent nodes to get the path
-            Node* current{ currentNode };
-            while (current != nullptr) {
-                path.push_back(current);
-                current = current->parent;
-            }
-            std::reverse(path.begin(), path.end());
-            break;
-        }
-
-        // Check neighbouring nodes
-        for (int neighbourY{ -1 }; neighbourY <= 1; ++neighbourY) {
-            for (int neighbourX{ -1 }; neighbourX <= 1; ++neighbourX) {
-                // Ignore if on the current node
-                if (neighbourX == 0 && neighbourY == 0)
-                    continue;
-
-                // Set variables for position of node to check
-                int checkX = currentNode->x + neighbourX;
-                int checkY = currentNode->y + neighbourY;
-
-                // Ignore if neighbour check goes out of range
-                if (checkX < 0 || checkX >= cols || checkY < 0 || checkY >= rows)
-                    continue;
-
-                // Ignore if neighbour is an obstacle (e.g. wall)
-                if (gameMap[checkX][checkY] == 1)
-                    continue;
-
-                //---CORNER CHECKS---//
-                // Top-left corner
-                if (neighbourX == -1 && neighbourY == -1) {
-                    // Edge on the right/bottom is an obstacle
-                    if (gameMap[checkX + 1][checkY] == 1 || gameMap[checkX][checkY + 1] == 1)
-                        continue;
-                }
-
-                // Top-right corner
-                if (neighbourX == 1 && neighbourY == -1) {
-                    // Edge on the left/bottom is an obstacle
-                    if (gameMap[checkX - 1][checkY] == 1 || gameMap[checkX][checkY + 1] == 1)
-                        continue;
-                }
-
-                // Bottom-left corner
-                if (neighbourX == -1 && neighbourY == 1) {
-                    // Edge on the top/right is an obstacle
-                    if (gameMap[checkX][checkY - 1] == 1 || gameMap[checkX + 1][checkY] == 1)
-                        continue;
-                }
-
-                // Bottom-right corner
-                if (neighbourX == 1 && neighbourY == 1) {
-                    // Edge on the top/left is an obstacle
-                    if (gameMap[checkX][checkY - 1] == 1 || gameMap[checkX - 1][checkY] == 1)
-                        continue;
-                }
-                //---CORNER CHECKS---//
-
-                // Initialize neighbour node
-                Node* neighbour = new Node(checkX, checkY);
-                neighbour->parent = currentNode;
-
-                // Add to node queue for further checking
-                nodeQueue.push(neighbour);
-            }
-        }
-    }
-
-    // Clean up memory for nodes.
-    while (!nodeQueue.empty()) {
-        delete nodeQueue.front();
-        nodeQueue.pop();
-    }
-
-    return path;
-}*/
-// FOR TESTING OF BFS //
-
-
-EnemyRoam::EnemyRoam() : bfs(new BFS(3, 5)), pathFound(false), printTest(false) {
-    wallPrinted = false;
-    index = 0;
-}
-//EnemyRoam::EnemyRoam() : pathFound(false), printTest(false) {}
+EnemyRoam::EnemyRoam() : bfs(new BFS(5, 5)), transitioned(false), pathFound(false), pathPrinted(false), pathIndex(0) {}
 
 void EnemyRoam::Update(size_t gameObjectID) {
-	//std::cout << "Does this work on " << objectFactory->GetGameObjectByID(gameObjectID)->GetName() << std::endl;
+	if (!transitioned) {
+		std::cout << "\nTRANSITIONED TO ROAM STATE" << std::endl;
+		transitioned = true;
+	}
 
     // Temporary variables
-	int startX = 1, startY = 1, targetX = 2, targetY = 0;
+	int startX = 1, startY = 0, targetX = 3, targetY = 4;
     
     // If grid not created, create grid
     if (!bfs->gridCreated) {
         bfs->CreateGrid();
     }
 
-
     if (!pathFound) {
         // Find path
-        //pathToTake = bfs->FindPath(1, 1, 2, 0);
 		pathToTake = bfs->FindPath(startX, startY, targetX, targetY);
 
         if (pathToTake.size() > 0) {
@@ -173,7 +43,7 @@ void EnemyRoam::Update(size_t gameObjectID) {
         }
     }
     else {
-        if (!printTest) {
+        if (!pathPrinted) {
             // Start traversing path
             std::cout << "Start\n";
             
@@ -183,42 +53,22 @@ void EnemyRoam::Update(size_t gameObjectID) {
             }
             std::cout << "End" << std::endl;
 
-            printTest = true;
-
-            /*for (Node* node : pathToTake) {
-                delete node;
-            }*/
-            //pathToTake.clear();
+            pathPrinted = true;
         }
 
-        //bfs->FollowPath(pathToTake, gameObjectID);
-
-		if (index < pathToTake.size()) {
+		if (pathIndex < pathToTake.size()) {
             // TEMPORARY VARIABLES
             float windowWidth = 1920.f;
             float windowHeight = 1017.f;
-            float scaleTemp = windowHeight / 3.f;
+            float scaleTemp = windowHeight / bfs->GetRows();
 
             // Enemy to traverse along path to take
             // While not within range of node, move towards node
             Vec2 currentEnemyPos = Vec2(0, 0);
             Vec2 nodePos;
-			Node currentNode = pathToTake[index];
+			Node currentNode = pathToTake[pathIndex];
             nodePos.x = (currentNode.x * scaleTemp) + (scaleTemp - windowWidth) / 2;
             nodePos.y = (currentNode.y * scaleTemp) + (scaleTemp - windowHeight) / 2;
-
-            /*if (!wallPrinted) {
-                // Create wall
-                GameObject* wall = objectFactory->BuildObjectRunTime("Wall", "Wall");
-                objectFactory->AddComponent(ComponentType::TRANSFORM, wall);
-
-                wall->Initialize();
-
-                // Set position of wall
-                GET_COMPONENT(wall, Transform, ComponentType::TRANSFORM)->scale = scaleTemp;
-                GET_COMPONENT(wall, Transform, ComponentType::TRANSFORM)->position.x = (currentNode.x * scaleTemp) + (scaleTemp - windowWidth) / 2;
-                GET_COMPONENT(wall, Transform, ComponentType::TRANSFORM)->position.y = (currentNode.y * scaleTemp) + (scaleTemp - windowHeight) / 2;
-            }*/
 
             GameObject* currentEnemy = objectFactory->GetGameObjectByID(gameObjectID);
             if (currentEnemy != nullptr) {
@@ -228,55 +78,49 @@ void EnemyRoam::Update(size_t gameObjectID) {
                 }
             }
 
-            if (Vector2DDistance(currentEnemyPos, nodePos) > 100) {
+            if (Vector2DDistance(currentEnemyPos, nodePos) > 5) {
                 Vec2 direction = Vec2(nodePos.x - currentEnemyPos.x, nodePos.y - currentEnemyPos.y);
                 Vector2DNormalize(direction, direction);
-                GET_COMPONENT(currentEnemy, PhysicsBody, ComponentType::PHYSICS_BODY)->velocity = direction * 200;
+                GET_COMPONENT(currentEnemy, PhysicsBody, ComponentType::PHYSICS_BODY)->velocity = direction * 300;
             }
 
             // continue if within range of next node
-            else if (Vector2DDistance(currentEnemyPos, nodePos) <= 100) {
+            else if (Vector2DDistance(currentEnemyPos, nodePos) <= 5) {
                 GET_COMPONENT(currentEnemy, PhysicsBody, ComponentType::PHYSICS_BODY)->velocity = Vec2(0, 0);
-                ++index;
-                std::cout << "PATHFINDING END" << std::endl;
+                ++pathIndex;
+                std::cout << "Travelled to node (" << currentNode.x << ", " << currentNode.y << ")" << std::endl;
             }
-            
 		}
     }
-    
+}
 
-    /*if (!pathFound) {
-        std::cout << "Start\n";
-        std::vector<Node*> path = BFS(0, 0, 6, 7);
-        // Print the path.
-        for (Node* node : path) {
-            std::cout << "(" << node->x << ", " << node->y << ") -> ";
-        }
-        std::cout << "End" << std::endl;
+void EnemyRoam::ExitState() {
+    transitioned = false; 
+    pathFound = false;
+    pathPrinted = false;
+    pathIndex = 0;
 
-        // Clean up memory.
-        for (Node* node : path) {
-            delete node;
-        }
-        pathFound = true;
-    }*/
+    pathToTake.clear();
+    //delete bfs;
 }
 
 EnemyRoam::~EnemyRoam() {
-    // Erase path vector
-    /*for (Node* node : pathToTake) {
-        delete node;
-    }*/
-    //pathToTake.clear();
-
-    // Delete bfs pointer
+    // Cleanup
+    pathToTake.clear();
 	delete bfs;
 }
 
-EnemyAttack::EnemyAttack() {}
+EnemyAttack::EnemyAttack() : transitioned(false) {}
 
 void EnemyAttack::Update(size_t gameObjectID) {
-	//std::cout << "TRANSITIONED TO ATTACK STATE LMAO\n" << std::endl;
+    if (!transitioned) {
+        std::cout << "\nTRANSITIONED TO ATTACK STATE\n" << std::endl;
+        transitioned = true;
+    }
+}
+
+void EnemyAttack::ExitState() {
+	transitioned = false;
 }
 
 EnemyAttack::~EnemyAttack() {
