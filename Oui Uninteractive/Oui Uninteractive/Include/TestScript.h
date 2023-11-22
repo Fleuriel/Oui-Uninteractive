fd974cd7b8a75fc16131bad8c85e209fe813ca0c
EmeryQuek <c.quek@digipen.edu>
2023-11-22 20:26:27 +0800

    rifle asset, pickup diff weapon

--- Oui Uninteractive/Oui Uninteractive/Include/TestScript.h
+++ Oui Uninteractive/Oui Uninteractive/Include/TestScript.h
@@ -246,14 +246,39 @@ public:
 			PhysicsBody* physBod = GET_COMPONENT(objectFactory->GetGameObjectByID(gameObjectID), PhysicsBody, ComponentType::PHYSICS_BODY);
 			if (tx != nullptr && physBod != nullptr) {
 				if (tx != nullptr && physBod != nullptr) {
-					PhysicsBody* playerBody = GET_COMPONENT(objectFactory->GetGameObjectByID(gameObjectID), PhysicsBody, ComponentType::PHYSICS_BODY);
+					//Set playerBody1 as the player
+					PhysicsBody* playerBody1 = GET_COMPONENT(objectFactory->GetGameObjectByID(0), PhysicsBody, ComponentType::PHYSICS_BODY);
+
+					//Set playerBody as the knife
+					PhysicsBody* playerBody = GET_COMPONENT(objectFactory->GetGameObjectByID(6), PhysicsBody, ComponentType::PHYSICS_BODY);
+					int tempo = Vector2DDistance(playerBody->txPtr->position, playerBody1->txPtr->position);
+
+					//Declare a temporary physics body
+					PhysicsBody* playerBodyZ;
+					PhysicsBody* playerBodyFinale;
+
+					for (int i = 7; i < 8; i++) {
+						
+						//Set playerBodyZ as the weapon after knife
+						playerBodyZ = GET_COMPONENT(objectFactory->GetGameObjectByID(i), PhysicsBody, ComponentType::PHYSICS_BODY);
+						int tempo2 = Vector2DDistance(playerBodyZ->txPtr->position, playerBody1->txPtr->position);
+
+						if (tempo < tempo2) {
+							playerBodyFinale = playerBody;
+						}
+						else {
+							playerBodyFinale = playerBodyZ;
+						}
+
+					}
+					//PhysicsBody* playerBody = GET_COMPONENT(objectFactory->GetGameObjectByID(7), PhysicsBody, ComponentType::PHYSICS_BODY);
 					static bool count = false;
 					if (count == true) {
 
-						PhysicsBody* playerBody1 = GET_COMPONENT(objectFactory->GetGameObjectByID(0), PhysicsBody, ComponentType::PHYSICS_BODY);
+						//PhysicsBody* playerBody1 = GET_COMPONENT(objectFactory->GetGameObjectByID(0), PhysicsBody, ComponentType::PHYSICS_BODY);
 
-						playerBody->txPtr->position.x = playerBody1->txPtr->position.x + 30;
-						playerBody->txPtr->position.y = playerBody1->txPtr->position.y - 20;
+						playerBodyFinale->txPtr->position.x = playerBody1->txPtr->position.x + 30;
+						playerBodyFinale->txPtr->position.y = playerBody1->txPtr->position.y - 20;
 						//playerBody->forceManager.ApplyToForce(Vec2(0.0f, 0.1f), 20.f, 0.3f, FORCE_INDEX::EXTERNAL);
 					}
 
@@ -263,19 +288,6 @@ public:
 					if (inputSystem.GetKeyState(GLFW_KEY_C)) {
 						count = 1;
 					}
-
-					/*if (inputSystem.GetKeyState(GLFW_KEY_X)) {
-						if (count == false) {
-							count = true;
-						}
-						else {
-							count = false;
-						} 
-						//count = !count;
-					}*/
-
-
-
 				}
 			}
 		}
