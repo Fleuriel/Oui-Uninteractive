148d00632f0e51f24f578e406df524c77d27eeec
EmeryQuek <c.quek@digipen.edu>
2023-11-23 20:37:17 +0800

    weaponpickup refined + removal of script 2 from certain scenes to prevent crash

--- Oui Uninteractive/Oui Uninteractive/Include/WeaponPickupScript.h
+++ Oui Uninteractive/Oui Uninteractive/Include/WeaponPickupScript.h
@@ -19,48 +19,56 @@ public:
 			PhysicsBody* physBod = GET_COMPONENT(objectFactory->GetGameObjectByID(gameObjectID), PhysicsBody, ComponentType::PHYSICS_BODY);
 			if (tx != nullptr && physBod != nullptr) {
 				if (tx != nullptr && physBod != nullptr) {
-					//Set playerBody1 as the player
-					PhysicsBody* playerBody1 = GET_COMPONENT(objectFactory->GetGameObjectByID(0), PhysicsBody, ComponentType::PHYSICS_BODY);
 
-					//Set playerBody as the knife
-					PhysicsBody* playerBody = GET_COMPONENT(objectFactory->GetGameObjectByID(1), PhysicsBody, ComponentType::PHYSICS_BODY);
-					int tempo = Vector2DDistance(playerBody->txPtr->position, playerBody1->txPtr->position);
-
-					//Declare a temporary physics body
-					PhysicsBody* playerBodyZ;
-					PhysicsBody* playerBodyFinale;
-
-					for (int i = 2; i < 3; i++) {
-
-						//Set playerBodyZ as the weapon after knife
-						playerBodyZ = GET_COMPONENT(objectFactory->GetGameObjectByID(i), PhysicsBody, ComponentType::PHYSICS_BODY);
-						int tempo2 = Vector2DDistance(playerBodyZ->txPtr->position, playerBody1->txPtr->position);
-
-						if (tempo < tempo2) {
-							playerBodyFinale = playerBody;
-						}
-						else {
-							playerBodyFinale = playerBodyZ;
-						}
-
-					}
-					//PhysicsBody* playerBody = GET_COMPONENT(objectFactory->GetGameObjectByID(7), PhysicsBody, ComponentType::PHYSICS_BODY);
 					static bool count = false;
-					if (count == true) {
-
-						//PhysicsBody* playerBody1 = GET_COMPONENT(objectFactory->GetGameObjectByID(0), PhysicsBody, ComponentType::PHYSICS_BODY);
-
-						playerBodyFinale->txPtr->position.x = playerBody1->txPtr->position.x + 30;
-						playerBodyFinale->txPtr->position.y = playerBody1->txPtr->position.y - 20;
-						//playerBody->forceManager.ApplyToForce(Vec2(0.0f, 0.1f), 20.f, 0.3f, FORCE_INDEX::EXTERNAL);
-					}
+					static bool pickedup = false;
+					int z = 1;
 
 					if (inputSystem.GetKeyState(GLFW_KEY_X)) {
 						count = 0;
+						pickedup = false;
+
 					}
 					if (inputSystem.GetKeyState(GLFW_KEY_C)) {
 						count = 1;
+						pickedup = true;
 					}
+
+					//Set playerBody1 as the player (permanent)
+					PhysicsBody* playerBody1 = GET_COMPONENT(objectFactory->GetGameObjectByID(0), PhysicsBody, ComponentType::PHYSICS_BODY);
+
+					//Declare a temporary physics body for the final weapon chosen
+					static PhysicsBody* playerBodyFinale = GET_COMPONENT(objectFactory->GetGameObjectByID(1), PhysicsBody, ComponentType::PHYSICS_BODY);
+
+					
+					if (pickedup == false) {
+						for (int i = 2; i < 4; i++) {
+							
+							PhysicsBody* playerBody = GET_COMPONENT(objectFactory->GetGameObjectByID(z), PhysicsBody, ComponentType::PHYSICS_BODY);
+							int tempo = Vector2DDistance(playerBody->txPtr->position, playerBody1->txPtr->position);
+							//Set playerBodyZ as the weapon after knife
+						    PhysicsBody* playerBodyZ = GET_COMPONENT(objectFactory->GetGameObjectByID(i), PhysicsBody, ComponentType::PHYSICS_BODY);
+							int tempo2 = Vector2DDistance(playerBodyZ->txPtr->position, playerBody1->txPtr->position);
+							
+							if (tempo < tempo2) {
+								playerBodyFinale = playerBody;
+							}
+							else {
+								playerBodyFinale = playerBodyZ;
+								tempo = tempo2;
+								playerBody = playerBodyZ;
+								z++;
+							}
+							
+						}
+					}
+
+					if (count == 1) {
+						playerBodyFinale->txPtr->position.x = playerBody1->txPtr->position.x + 30;
+						playerBodyFinale->txPtr->position.y = playerBody1->txPtr->position.y - 20;
+					}
+
+
 				}
 			}
 		}
