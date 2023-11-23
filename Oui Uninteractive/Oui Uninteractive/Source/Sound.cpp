09c12e744686bda67f341597ff8989d2999f73a1
Markkeroni <112945155+Markkeroni@users.noreply.github.com>
2023-11-23 19:15:49 +0800

    Sound System revamp

--- Oui Uninteractive/Oui Uninteractive/Source/Sound.cpp
+++ Oui Uninteractive/Oui Uninteractive/Source/Sound.cpp
@@ -41,7 +41,7 @@ void SoundManager::Initialize() {
 		std::cout << "FMOD error: " << FMOD_ErrorString(result);
 	}
 	// Initailize FMOD
-	result = system->init(10, FMOD_INIT_NORMAL, 0); // 10 max channels set
+	result = system->init(512, FMOD_INIT_NORMAL, 0); // 100 max channels set
 	if (result != FMOD_OK) {
 		std::cout << "FMOD error: " << FMOD_ErrorString(result);
 	}
@@ -56,71 +56,110 @@ void SoundManager::Initialize() {
 void SoundManager::Update(float dt) {
 	// Start time profiling for sound system
 	TimeProfiler profiler(Editor::timeRecorder.soundTime);
-	
-	//std::chrono::high_resolution_clock::time_point timeStart = std::chrono::high_resolution_clock::now();
-	
+		
 	system->update();
 
-	//std::chrono::high_resolution_clock::time_point timeEnd = std::chrono::high_resolution_clock::now();
-	//std::chrono::duration<float, std::milli> duration = timeEnd - timeStart;
-	//Editor::timeRecorder.soundTime = duration.count();
+	// Check which channel is still playing for automatic removal once stopped
+	std::vector<int> removeCHs;
+	for (auto& [id, channel] : soundChannels) {
+		bool isPlaying = false;
+		channel->isPlaying(&isPlaying);
+		if (!isPlaying) {
+			removeCHs.push_back(id);
+		}
+	}
+	// Recycle channels to pool
+	for (int id : removeCHs) {
+		soundChannels.erase(id);
+		recycleChannelID(id);
+	}
 	(void)dt;
 }
 
 
-/**************************************************************************
-* @brief This function plays the BGM sounds
-* @return No return
-*************************************************************************/
-void SoundManager::PlayBGMSounds() {
-	// Play BGM once clicked
-	bool playStatus1, playStatus2;
-	bgmChannels[0]->isPlaying(&playStatus1);
-	if (bgmChannels[0] == nullptr || !playStatus1) {
-		result = system->playSound(assetManager.GetBGM("Nightshift__BGM2_Loop_70bpm.wav"), nullptr, true, &bgmChannels[0]);
-		if (result != FMOD_OK) {
-			std::cout << "FMOD error: " << FMOD_ErrorString(result);
-			return;
-		}
-	}
+
+int SoundManager::getAvailableChannelID() {
+	static int currID = 0;
 	
-	bgmChannels[1]->isPlaying(&playStatus2);
-	if (bgmChannels[1] == nullptr || !playStatus2) {
-		result = system->playSound(assetManager.GetBGM("Suspense.wav"), nullptr, true, &bgmChannels[1]);
-		if (result != FMOD_OK) {
-			std::cout << "FMOD error: " << FMOD_ErrorString(result);
-			return;
-		}
+	if (!availableIDs.empty()) {
+		int id = availableIDs.front();
+		availableIDs.pop();
+		return id;
 	}
-	
+	return currID++;
 }
 
 
-/**************************************************************************
-* @brief This function plays the SFX sounds
-* @return No return
-*************************************************************************/
-void SoundManager::PlaySFXSounds() {
-	// Play SFX once clicked
-	sfxChannels[0]->stop();
-	result = system->playSound(assetManager.GetSFX("Gunshot.wav"), nullptr, false, &sfxChannels[0]);
+void SoundManager::recycleChannelID(int id) {
+	availableIDs.push(id);
+}
+
+
+void SoundManager::PlaySFX(const std::string& sound) {
+	FMOD::Sound* sfxSound = assetManager.GetSFX(sound);
+	FMOD::Channel* channel;
+	result = system->playSound(sfxSound, soundGroups[SoundGroup::SGSFX], false, &channel);
 	if (result != FMOD_OK) {
 		std::cout << "FMOD error: " << FMOD_ErrorString(result);
+		return;
 	}
+	int channelID = getAvailableChannelID();
+	soundChannels[channelID] = channel;
 }
 
-
-
-/**************************************************************************
-* @brief These functions handle pausing the channels
-* @param selectedChannel - Pointer to a FMOD channel to play sound to
-* @return No return
-*************************************************************************/
-void SoundManager::TogglePlayChannel(FMOD::Channel* selectedChannel) {
-	bool pausedState;
-	selectedChannel->getPaused(&pausedState);
-	selectedChannel->setPaused(!pausedState);
-}
+//**************************************************************************
+//* @brief This function plays the BGM sounds
+//* @return No return
+//*************************************************************************/
+//void SoundManager::PlayBGMSounds() {
+//	// Play BGM once clicked
+//	bool playStatus1, playStatus2;
+//	bgmChannels[0]->isPlaying(&playStatus1);
+//	if (bgmChannels[0] == nullptr || !playStatus1) {
+//		result = system->playSound(assetManager.GetBGM("Nightshift__BGM2_Loop_70bpm.wav"), nullptr, true, &bgmChannels[0]);
+//		if (result != FMOD_OK) {
+//			std::cout << "FMOD error: " << FMOD_ErrorString(result);
+//			return;
+//		}
+//	}
+//	
+//	bgmChannels[1]->isPlaying(&playStatus2);
+//	if (bgmChannels[1] == nullptr || !playStatus2) {
+//		result = system->playSound(assetManager.GetBGM("Suspense.wav"), nullptr, true, &bgmChannels[1]);
+//		if (result != FMOD_OK) {
+//			std::cout << "FMOD error: " << FMOD_ErrorString(result);
+//			return;
+//		}
+//	}
+//	
+//}
+//
+//
+///**************************************************************************
+//* @brief This function plays the SFX sounds
+//* @return No return
+//*************************************************************************/
+//void SoundManager::PlaySFXSounds() {
+//	// Play SFX once clicked
+//	sfxChannels[0]->stop();
+//	result = system->playSound(assetManager.GetSFX("Gunshot.wav"), nullptr, false, &sfxChannels[0]);
+//	if (result != FMOD_OK) {
+//		std::cout << "FMOD error: " << FMOD_ErrorString(result);
+//	}
+//}
+//
+//
+//
+///**************************************************************************
+//* @brief These functions handle pausing the channels
+//* @param selectedChannel - Pointer to a FMOD channel to play sound to
+//* @return No return
+//*************************************************************************/
+//void SoundManager::TogglePlayChannel(FMOD::Channel* selectedChannel) {
+//	bool pausedState;
+//	selectedChannel->getPaused(&pausedState);
+//	selectedChannel->setPaused(!pausedState);
+//}
 
 
 /**************************************************************************
