ade67487c06bd3dd742af438b092ff96411db2cb
Austin Hwang <hwangjingrui@gmail.com>
2023-11-25 18:12:48 +0800

    Added health component/system + bullet changes
    when bullet hits common guard or target, it destroys itself now

---
+++ Oui Uninteractive/Oui Uninteractive/Include/HealthComponent.h
@@ -0,0 +1,53 @@
+/**************************************************************************
+ * @file HealthComponent.h
+ * @author HWANG Jing Rui, Austin
+ * @par DP email: jingruiaustin.hwang@digipen.edu
+ * @par Course:	CSD 2401
+ * @par Software Engineering Project 3
+ * @date 25-11-2023
+ * @brief This file contains the declaration of the health component.
+ *************************************************************************/
+#ifndef HEALTHCOMPONENT_H
+#define HEALTHCOMPONENT_H
+
+#include <map>
+#include <string>
+#include "IComponent.h"
+
+class HealthComponent : public IComponent {
+private:
+	int maxHealth;
+	int currentHealth;
+
+public:
+	/**************************************************************************
+	* @brief Constructor
+	*************************************************************************/
+	HealthComponent();
+
+	/**************************************************************************
+	* @brief Destructor
+	*************************************************************************/
+	~HealthComponent();
+
+	/**************************************************************************
+	* @brief Initialize the health component
+	* @return void
+	*************************************************************************/
+	void Initialize() override;
+
+	/**************************************************************************
+	* @brief Serialize health component data from JSON
+	* @param itr - iterator for JSON object
+	* @return void
+	*************************************************************************/
+	void Serialize(rapidjson::Value::ConstMemberIterator& itr) override;
+
+	/**************************************************************************
+	* @brief Clone health component
+	* @return HealthComponent* - pointer to cloned health component
+	*************************************************************************/
+	HealthComponent* Clone() const override;
+};
+
+#endif
---
+++ Oui Uninteractive/Oui Uninteractive/Source/HealthSystem.cpp
@@ -0,0 +1,49 @@
+/**************************************************************************
+ * @file EnemyAISystem.h
+ * @author HWANG Jing Rui, Austin
+ * @par DP email: jingruiaustin.hwang@digipen.edu
+ * @par Course:	CSD 2401
+ * @par Software Engineering Project 3
+ * @date 08-11-2023
+ * @brief This file contains the definition of the EnemyAISystem class.
+ *		  This system handles the state machine for the enemies.
+ *************************************************************************/
+#include "HealthSystem.h"
+#include "ComponentFactory.h"
+//#include "Transform.h"
+//#include "Vector2D.h"
+
+HealthSystem* healthSystem = nullptr;
+
+/**************************************************************************
+* @brief Constructor
+*************************************************************************/
+HealthSystem::HealthSystem() {
+	if (healthSystem != nullptr) {
+		return;
+	}
+	else {
+		healthSystem = this;
+	}
+}
+
+/**************************************************************************
+* @brief Initialize the health system
+* @return void
+*************************************************************************/
+void HealthSystem::Initialize() {
+	ComponentFactory<HealthComponent>* cfPtr{ new ComponentFactory<HealthComponent>(ComponentType::HEALTH) };
+	objectFactory->AddComponentFactory(ComponentType::HEALTH, cfPtr);
+}
+
+/**************************************************************************
+* @brief Update the health system
+* @return void
+*************************************************************************/
+void HealthSystem::Update(float dt) {
+	(void)dt;
+
+	for (auto& it : healthComponentMap) {
+		
+	}
+}
