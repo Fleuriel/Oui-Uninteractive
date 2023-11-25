cdcc7768881352da68357bdf3a06f9777ea9becf
Sawkon <aaronjunxiang.chan@digipen.edu>
2023-11-25 17:19:55 +0800

    fullscreen

--- Oui Uninteractive/Oui Uninteractive/Source/OpenGLApplication.cpp
+++ Oui Uninteractive/Oui Uninteractive/Source/OpenGLApplication.cpp
@@ -121,6 +121,8 @@ void OpenGLApplication::OpenGLWindowInit() {
 		return;
 	}
 
+	toggleFullScreen();
+
 	// Tell GLFW we are using OpenGL 4.5
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
