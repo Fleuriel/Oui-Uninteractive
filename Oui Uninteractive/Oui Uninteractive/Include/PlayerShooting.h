8ccdb3d2feefb196b2cf2d7a7a8c048525b7a026
Austin Hwang <hwangjingrui@gmail.com>
2023-11-26 19:32:42 +0800

    push first, but enemies still take double damage

--- Oui Uninteractive/Oui Uninteractive/Include/PlayerShooting.h
+++ Oui Uninteractive/Oui Uninteractive/Include/PlayerShooting.h
@@ -84,7 +84,7 @@ public:
 				// Set bullet spawn point
 				bulletSpawnAngle = atan2(mousePos.y - playerPos.y, mousePos.x - playerPos.x);
 				bulletSpawnOffset = (playerScaleX >= playerScaleY) ? playerScaleX : playerScaleY;
-				bulletSpawnOffset *= 1.1f;
+				bulletSpawnOffset *= 0.75f;
 				bulletSpawnPos.x = playerPos.x + bulletSpawnOffset * cos(bulletSpawnAngle);
 				bulletSpawnPos.y = playerPos.y + bulletSpawnOffset * sin(bulletSpawnAngle);
 				GET_COMPONENT(bullet, Transform, ComponentType::TRANSFORM)->position = bulletSpawnPos;
