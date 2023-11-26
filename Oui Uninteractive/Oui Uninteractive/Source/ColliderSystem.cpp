09ca44e3f98cfc06d439a212f173b87dc8011394
Austin Hwang <hwangjingrui@gmail.com>
2023-11-26 17:09:52 +0800

    fix crash

--- Oui Uninteractive/Oui Uninteractive/Source/ColliderSystem.cpp
+++ Oui Uninteractive/Oui Uninteractive/Source/ColliderSystem.cpp
@@ -76,10 +76,10 @@ void ColliderSystem::Update(float dt) {
 						if (collided) {
 							didCollide = true;
 							if (pBody1->GetOwner()->GetType() == "BulletPrefab") {
+								objectFactory->DestroyObject(pBody1->GetOwner());
 								if (pBody2->GetOwner()->GetType() == "Enemy") {
 									--GET_COMPONENT(pBody2->GetOwner(), HealthComponent, ComponentType::HEALTH)->currentHealth;
 								}
-								objectFactory->DestroyObject(pBody1->GetOwner());
 							}
 							if (pBody2->GetOwner()->GetType() == "BulletPrefab") {
 								objectFactory->DestroyObject(pBody2->GetOwner());
@@ -122,69 +122,6 @@ void ColliderSystem::Update(float dt) {
 					collider->contactTime = 1.f;
 					collider->contactNormal = Vec2(0, 0);
 				}
-				PhysicsBody* pBody1 = GET_COMPONENT(collider->GetOwner(), PhysicsBody, ComponentType::PHYSICS_BODY);
-				PhysicsBody* pBody2 = GET_COMPONENT(body2->GetOwner(), PhysicsBody, ComponentType::PHYSICS_BODY);
-				float contactTime = 0;
-				bool collided = false;
-				Vec2 normal = Vec2(0, 0);
-
-				if (pBody1 != nullptr && pBody2 != nullptr) {
-
-					Vec2 nextCycleVel = pBody1->velocity + (pBody1->forceManager.CalculateResultantForce() * pBody1->mass) * static_cast<float>(sysManager->fixedDeltaTime);
-				Vec2 nextCycleVel2 = pBody2->velocity + (pBody2->forceManager.CalculateResultantForce() * pBody2->mass) * static_cast<float>(sysManager->fixedDeltaTime);
-
-				Vec2 relVel = nextCycleVel - nextCycleVel2;
-
-				collided = CollisionMovingRectRect(*(collider->boundingbox), *(body2->boundingbox), relVel, contactTime, normal, static_cast<float>(sysManager->fixedDeltaTime), nextCycleVel);
-				//Collision Response
-				if (collided) {
-					didCollide = true;
-					if (pBody1->GetOwner()->GetType() == "BulletPrefab"){	
-						objectFactory->DestroyObject(pBody1->GetOwner());
-						if (pBody2->GetOwner()->GetType() == "Enemy") {
-							--GET_COMPONENT(pBody2->GetOwner(), HealthComponent, ComponentType::HEALTH)->currentHealth;
-						}
-					}
-					else if (pBody2->GetOwner()->GetType() == "BulletPrefab") {
-						objectFactory->DestroyObject(pBody2->GetOwner());
-						if (pBody1->GetOwner()->GetType() == "Enemy") {
-							--GET_COMPONENT(pBody1->GetOwner(), HealthComponent, ComponentType::HEALTH)->currentHealth;
-						}
-					}
-					else {
-						if (!pBody2->isStatic) {
-							//both objects can move
-							float secondNorm = 0.f;
-							float dp = Vector2DDotProduct(nextCycleVel, nextCycleVel2);
-							if (dp > 0) {
-								secondNorm = 1.f;
-							}
-							pBody1->forceManager.DeactivateForce(0);
-							pBody1->forceManager.DeactivateForce(1);
-							pBody2->forceManager.DeactivateForce(0);
-							pBody2->forceManager.DeactivateForce(1);
-							CollisionMessage collisionMessage(collider, body2, contactTime, normal, secondNorm, -normal);
-							SendToObservers(&collisionMessage);
-
-						}
-						else {
-							//coll response
-							if (pBody2->isStatic) {
-								pBody1->forceManager.DeactivateForce(0);
-								pBody1->forceManager.DeactivateForce(1);
-								CollisionMessage collisionMessage(collider, body2, contactTime, normal, 1.f, normal);
-								SendToObservers(&collisionMessage);
-							}
-						}
-					}
-					
-				}//Collision Response end		
-			}
-			}
-			//reset collision data if no collision detected with anything
-			if (didCollide == false) {
-				collider->contactTime = 1.f;
-				collider->contactNormal = Vec2(0,0);
 			}
 		}
 	}
