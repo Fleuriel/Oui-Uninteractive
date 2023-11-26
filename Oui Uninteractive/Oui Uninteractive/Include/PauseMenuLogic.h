7b0bb4e69a85da7af8d4f36af12e1892ed3d17c5
TristanCheah <cheahtristan@Hotmail.sg>
2023-11-26 22:00:07 +0800

    new bg

--- Oui Uninteractive/Oui Uninteractive/Include/PauseMenuLogic.h
+++ Oui Uninteractive/Oui Uninteractive/Include/PauseMenuLogic.h
@@ -13,12 +13,13 @@ class PauseMenuLogic : public IScript {
 			
 		}
 		void Update(size_t gameObjectID) {
+			fontManager->RenderText("Next_Sunday.ttf", "Resume", 500, 500, 1.0f, glm::vec3(236.0f / 255.0f, 1.0f, 220.0f / 255.0f));
 			if (sysManager->isPaused) {
 				double mouseX; // = io.MousePos.x;
 				double mouseY; // = io.MousePos.y;
 
 				glfwGetCursorPos(windowNew, &mouseX, &mouseY);
-				background.SetBackGround(assetManager.GetTexture("Background3"));
+				background.SetBackGround(assetManager.GetTexture("GreenBG1920"));
 				OpenGLObject::FrameBufferMouseCoords(windowNew, &mouseX, &mouseY, OpenGLObject::cameraObject);
 				GameObject* gObj = objectFactory->GetGameObjectByID(gameObjectID);
 				if (gObj != nullptr) {
@@ -28,6 +29,7 @@ class PauseMenuLogic : public IScript {
 							if (gObj->GetName() == "Resume") {
 								//position game object in camera
 								tx->position = Vec2(OpenGLObject::cameraObject.posX - Editor::gameWindowSize.first / 4.f, OpenGLObject::cameraObject.posY);
+								
 							}
 							else if (gObj->GetName() == "Quit") {
 								tx->position = Vec2(OpenGLObject::cameraObject.posX + Editor::gameWindowSize.first / 4.f , OpenGLObject::cameraObject.posY);
